20465b8f5c29652fe76f35f35e6d14141c2563dd
Grathwohl, Philipp <philipp.grathwohl@chg-meridian.com>
2023-04-25 14:34:24 +0200

    Include powershell scripts to install as service

--- src/seq-exporter/InstallAsWindowsService.ps1
+++ src/seq-exporter/InstallAsWindowsService.ps1
@@ -1,2 +1,2 @@
-$Command = 'sc.exe create "{0}" binpath= "{1}" displayname= "{2}" start= "delayed-auto"' -f "SeqExporter","$(pwd)/seq_exporter.exe","Seq query metrics exporter"
+$Command = 'sc.exe create "{0}" binpath= "{1}" displayname= "{2}" start= "delayed-auto"' -f "SeqExporter","$(pwd)/seq-exporter.exe","Seq query metrics exporter"
 Invoke-Expression $Command
