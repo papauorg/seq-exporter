f42fe900f9d1045646e19eea3b8952c6f2c8ec36
Grathwohl, Philipp <philipp.grathwohl@chg-meridian.com>
2023-05-05 11:30:25 +0200

    Allow specifying additional labels per query

--- src/seq-exporter/Options/QueryDefinition.cs
+++ src/seq-exporter/Options/QueryDefinition.cs
@@ -7,4 +7,5 @@ public record QueryDefinition
     public string MetricName { get; init; } = "";
     public string MetricUnit { get; init; } = "";
     public string MetricDescription { get; init; } = "";
+    public Dictionary<string, string> AdditionalLabels { get; init; } = new Dictionary<string, string>();
 }
